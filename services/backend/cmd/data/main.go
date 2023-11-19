84f5e1133ec48cbfd30413cbc0d00b09509a34e3
fcmdias <fernando.cesardias7@yahoo.com>
2023-11-19 17:44:35 +0000

    added graceful shutdown to the api and rearranged repo folders

--- services/backend/cmd/data/main.go
+++ services/backend/cmd/data/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"context"
 	"github/com/fcmdias/CSVAnalysis/services/backend/pkg/web"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
-	http.Handle("/popular", web.EnableCORSMiddleware(http.HandlerFunc(web.PopularHandler)))
-	http.Handle("/byyear", web.EnableCORSMiddleware(http.HandlerFunc(web.ByYearHandler)))
-	log.Println("Starting server on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	router := http.NewServeMux()
+	router.Handle("/popular", web.EnableCORSMiddleware(http.HandlerFunc(web.PopularHandler)))
+	router.Handle("/byyear", web.EnableCORSMiddleware(http.HandlerFunc(web.ByYearHandler)))
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	go func() {
+		log.Println("Starting server on :8080")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	<-stopChan
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	log.Println("Server gracefully stopped")
 }
